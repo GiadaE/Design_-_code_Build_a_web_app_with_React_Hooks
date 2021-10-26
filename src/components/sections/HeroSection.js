739f5f2162c72adaa6c6305779b282e8ce356c7c
GiadaE <52630377+GiadaE@users.noreply.github.com>
2021-10-27 00:22:17 +0200

    first commit

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,104 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import MockupAnimation from "../animations/MockupAnimation";
+import WaveBackground from "../backgrounds/WaveBackground";
+import PurchaseButton from "../buttons/PurchaseButton";
+import { themes } from "../styles/ColorStyles";
+import { H1, MediumText } from "../styles/TextStyles";
+
+export default function HeroSection() {
+  return (
+    <Wrapper>
+        <WaveBackground />
+        <ContentWrapper>
+            <TextWrapper>
+                <Title>Design
+                    <br/> and code <span>React</span> apps
+                </Title>
+                <Description>
+                    Don’t skip design. Learn design and code, by building real apps with
+                    React and Swift. Complete courses about the best tools.
+                </Description>
+                <PurchaseButton title='Start learning' subtitle='120+ hours of video'/>
+            </TextWrapper>
+            <MockupAnimation />
+        </ContentWrapper>
+    </Wrapper>
+  )
+}
+
+const animation = keyframes`
+    0% { opacity: 0; transform: translate(-10px); filter: blur(10px) }
+    /* 80% { opacity: 0.5; transform: translate(-10px); filter: blur(10px) } */
+    100% { opacity: 1; transform: translate(0px); filter: blur(0px) }
+`
+
+const Wrapper = styled.div`
+    /* is like "Cut content" in Figma */
+    overflow: hidden;
+`
+
+const ContentWrapper = styled.div`
+    max-width: 1234px;
+    margin: 0 auto;
+    padding: 200px 30px;
+    display: grid;
+    grid-template-columns: 360px auto;
+
+    @media (max-width: 450px) {
+        grid-template-columns: auto;
+        gap: 60px;
+        padding: 150px 20px 250px;
+    }
+`
+
+const TextWrapper = styled.div`
+    max-width: 360px;
+    display: grid;
+    gap: 30px; 
+
+    > * {
+        opacity: 0;
+        animation: ${animation} 1s forwards;
+
+        :nth-child(1) {
+            animation-delay: 0s;
+        }
+        :nth-child(2) {
+            animation-delay: 0.2s;
+        }
+        :nth-child(3) {
+            animation-delay: 0.4s;
+        }
+    }
+`
+
+/* include reusable styled component*/
+const Title = styled(H1)`
+    color:${themes.dark.text1};
+    background: linear-gradient(180deg, #730040 0%, #301CBE 100%);
+    background-clip: text;
+    /* for safari and chrome */
+    -webkit-background-clip: text;
+    color: transparent;
+
+    span {
+        background: linear-gradient(180deg, #FFD7FF 0%, #FFB6FF 100%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+    }
+
+    @media (max-width: 450px) {
+        /* because there are three lines of title */
+        font-size: 48px;
+    }
+`
+
+const Description = styled(MediumText)`
+    font-size: 17px;
+    line-height: 130%;
+`
+
+
+
